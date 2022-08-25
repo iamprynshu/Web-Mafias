1e4c2cf332fc023b65d09ee3958e6051283be99b
Priyanshu Sharma <85369616+iamprynshu@users.noreply.github.com>
2022-08-25 18:38:20 +0530

    Put some links

--- src/containers/instructor/Instructor.jsx
+++ src/containers/instructor/Instructor.jsx
@@ -15,10 +15,10 @@ const Instructor = () => {
         <h1 className="gradient__text">Hitesh <br /> Chaudhary</h1>
         <p>I like to make videos related to code and tech in my free time. I also lead a few tech teams in startups, help in hiring talent for companies. I am also on a part time traveller, with 31 countries checked off so far!</p>
         <div className='web__instructor-icons'>
-          <a href="/" class="fa fa-twitter"></a>
-          <a href="/" class="fa fa-youtube"></a>
-          <a href="/" class="fa fa-github"></a>
-          <a href="/" class="fa fa-facebook"></a>
+          <a href="https://twitter.com/hiteshdotcom?lang=en" class="fa fa-twitter"></a>
+          <a href="https://www.youtube.com/channel/UCXgGY0wkgOzynnHvSEVmE3A" class="fa fa-youtube"></a>
+          <a href="https://github.com/hiteshchoudhary" class="fa fa-github"></a>
+          <a href="https://www.facebook.com/HiteshChoudharyPage" class="fa fa-facebook"></a>
         </div>
       </div>
 
